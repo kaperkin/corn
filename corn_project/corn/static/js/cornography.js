1fec0b723dd8575727f3134478ed9cd7fb83ab29
kaperkin <kaleena.perkins@gmail.com>
2015-09-02 19:57:33 -0700

    added jquery to rehide pictures

--- corn_project/corn/static/js/cornography.js
+++ corn_project/corn/static/js/cornography.js
@@ -1,25 +1,40 @@
-/*hiding cornography image until clicked*/
-/*hide image*/
-$('.image').hide();
-/*create button*/
-$('.censor').append('<button></button>');
-/* when button clicked */
-$('button').click(function(){
-/* show image */
-	$(this).prev().show();
-/* remove button */
-	$(this).remove();
+
+	/*hiding cornography image until clicked*/
+	/*hide image*/
+	$('.image').hide();
+	/*create button*/
+	$('.censor').append('<button></button>');
+	/* when button clicked */
+	$('button').click(function () {
+		/* show image */
+		$(this).prev().show();
+		/* remove button */
+		$(this).hide();
 	});
 
-/*hiding Shy Corn cornography image until clicked*/
-/*hide image*/
-$('#image-long').hide();
-/*create button*/
-$('#censor-long').append('<button id = "button-long"></button>');
-/* when button clicked */
-$('button').click(function(){
-/* show image */
-	$(this).prev().show();
-/* remove button */
-	$(this).remove();
+	$('.image').click(function () {
+		/*create button*/
+		$(this).parent().append('<button></button>');
+		$(this).hide();
 	});
+
+	/*hiding Shy Corn cornography image until clicked*/
+	/*hide image*/
+	$('#image-long').hide();
+	/*create button*/
+	$('#censor-long').append('<button id = "button-long"></button>');
+	/* when button clicked */
+	$('button').click(function () {
+		/* show image */
+		$(this).prev().show();
+		/* remove button */
+		$(this).remove();
+	});
+
+		$('#image-long').click(function () {
+		/*create button*/
+		$(this).parent().append('<button id ="button-long"></button>');
+		$(this).hide();
+	});
+
+
